87dc103c33f23419ef5ef33a278d6536bbab5e3e
Cipher-Git <globalcipher@gmail.com>
2022-01-27 12:26:30 +0600

    Добавьте файлы проекта.

---
+++ RDP Server/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace RDP_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.labelName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Server key connect";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelName.Location = new System.Drawing.Point(29, 70);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(41, 15);
+            this.labelName.TabIndex = 6;
+            this.labelName.Text = "Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(169, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 52);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(114)))), ((int)(((byte)(174)))));
+            this.label1.Location = new System.Drawing.Point(28, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "RDP Viewer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 9;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 75);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(251, 94);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(267, 133);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Server";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ RDP Server/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Windows.Forms;
+using RDPCOMAPILib;
+using System.IO;
+using Microsoft.Win32;
+using System.Configuration;
+//using WinSCP;
+using System.Drawing;
+
+namespace RDP_Server
+{
+
+    public partial class Form1 : Form
+    {
+        public static RDPSession currentSession = null;
+
+        public static void createSession()
+        {
+
+            currentSession = new RDPSession();
+
+        }
+
+        public void Connect(RDPSession session)
+        {
+            
+                session.OnAttendeeConnected += Incoming;
+                session.Open();
+            
+            
+        }
+
+        public static void Disconnect(RDPSession session)
+        { 
+            session.Close();
+        }
+
+        public static string GetConnectionString(RDPSession session, String authString, string group, string password, int clientLimit)
+        {
+            IRDPSRAPIInvitation invitation = session.Invitations.CreateInvitation
+            (authString, group, password, clientLimit);
+            return invitation.ConnectionString;
+        }
+
+        private static void Incoming(object Guest)
+        {
+            IRDPSRAPIAttendee MyGuest = (IRDPSRAPIAttendee)Guest;
+            MyGuest.ControlLevel = CTRL_LEVEL.CTRL_LEVEL_MAX;
+
+        }
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            string ExePath = Application.ExecutablePath;
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            //RegistryKey reg_1 = Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Run");
+            reg.SetValue("RDP Server.exe", ExePath);
+            reg.Close();
+
+            notifyIcon1.Visible = false; 
+            this.notifyIcon1.MouseDoubleClick += new MouseEventHandler(notifyIcon1_MouseClick);
+            this.Resize += new EventHandler(this.Form1_Resize);
+            labelName.Text = "Name ID:        " + Environment.MachineName;
+
+            timer.Tick += new EventHandler(RefreshLabel);
+            timer.Interval = 5000; // Здесь измени интервал на 5000 (5 сек)
+            timer.Start();
+        }
+
+
+        Timer timer = new Timer();
+
+        public void RefreshLabel(object sender, EventArgs e)
+        {
+            if (Internet.CheckConnection())
+            {
+                label2.Text = "INTERNET OK";
+                pictureBox2.BackColor = Color.FromArgb(57, 180, 74);
+                if (currentSession == null)
+                {
+                    System.Threading.Thread.Sleep(5000);
+                    StartServer();
+                }
+
+            }
+            else 
+            {
+                label2.Text = "INTERNET ERROR";
+                pictureBox2.BackColor = Color.FromArgb(255, 56, 37); 
+                if (currentSession != null)
+                {
+                    //Disconnect(currentSession);
+                    Application.Restart();
+                }
+
+            }
+
+        }
+
+        //private static int WinScpConect()
+        //{
+        //    try
+        //    {
+        //        string pathIp = ConfigurationManager.AppSettings.Get("IP");
+        //        // Setup session options
+        //        SessionOptions sessionOptions = new SessionOptions
+        //        {
+        //            Protocol = Protocol.Sftp,
+        //            HostName = pathIp,
+        //            UserName = "server",
+        //            Password = "password",
+        //            SshHostKeyFingerprint = "ssh-**************************************************",
+        //        };
+
+        //        using (Session session = new Session())
+        //        {
+        //            // Connect
+        //            session.Open(sessionOptions);
+
+        //            // Download files
+        //            TransferOptions transferOptions = new TransferOptions();
+        //            transferOptions.TransferMode = TransferMode.Binary;
+
+        //            TransferOperationResult transferResult;
+        //            string pathDir = ConfigurationManager.AppSettings.Get("dir");
+        //            transferResult = session.PutFiles(pathDir, @"/KEY", false, transferOptions);
+
+        //            // Throw on any error
+        //            transferResult.Check();
+
+        //            // Print results
+        //            foreach (TransferEventArgs transfer in transferResult.Transfers)
+        //            {
+        //                Console.WriteLine("Download of {0} succeeded", transfer.FileName);
+        //            }
+        //        }
+
+        //        return 0;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Error: {0}", e);
+        //        return 1;
+        //    }
+        //}
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+
+        }
+
+
+        private void StartServer()
+        {
+            //Disconnect(currentSession);
+            createSession();
+            Connect(currentSession);
+
+            string pathDir = ConfigurationManager.AppSettings.Get("dir");
+            string pathPass = ConfigurationManager.AppSettings.Get("Pass");
+            DirectoryInfo dirInfo = new DirectoryInfo(pathDir);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            string serverName = Environment.MachineName;
+            string key = GetConnectionString(currentSession, serverName, "", pathPass, 5);
+
+            File.WriteAllText(pathDir + serverName + ".bin", key);
+
+           // WinScpConect();
+        }
+        
+    }
+}
+
